9114bbdf4facc01f6f9d2a289fa577dd64e598f2
Justin Sixsmith <justinsixsmith@gmail.com>
2022-02-22 11:00:58 -0600

    populates weird cards

--- movies-js.js
+++ movies-js.js
@@ -8,26 +8,55 @@ const url = "https://celestial-dramatic-fuschia.glitch.me/movies";
 function fetchAllMovies() {
     return fetch(url)
         .then(res => res.json()
-            .then(data => data))
+            .then(data => {
+                console.log(data);
+                $("#card-area").html("");
+                for (const movie of data) {
+                    let title = movie.title;
+                    let movieCard = `<div class="col mb-5">
+                                        <div class="card h-100">
+                                            <!-- Product image-->
+                                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..."/>
+                                            <!-- Product details-->
+                                            <div class="card-body p-4">
+                                                <div class="text-center">
+                                                    <!-- Product name-->
+                                                    <h5 class="fw-bolder">${title}</h5>
+                                                    <!-- Product price-->
+                                                    $40.00 - $80.00
+                                                </div>
+                                            </div>
+                                            <!-- Product actions-->
+                                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">View options</a></div>
+                                            </div>
+                                        </div>
+                                    </div>`
+
+
+
+                    $("#card-area").append(movieCard);
+                }
+            }))
             .catch(err => console.log(err));
 }
 
 
 // GET ONE MOVIE
-function fetchOneMovie(movieID) {
-    return fetch(url)
-        .then(res => res.json())
-        .then(movies => {
-            for (let movie of movies) {
-                if (movie.id === movieID) {
-                    return movie;
-                }
-            }
-        })
-        .catch(err => console.log(err));
-}
-
-fetchOneMovie(5).then(movieInfo => console.log(movieInfo));
+// function fetchOneMovie(movieID) {
+//     return fetch(url)
+//         .then(res => res.json())
+//         .then(movies => {
+//             for (let movie of movies) {
+//                 if (movie.id === movieID) {
+//                     return movie;
+//                 }
+//             }
+//         })
+//         .catch(err => console.log(err));
+// }
+//
+// fetchOneMovie(5).then(movieInfo => console.log(movieInfo));
 
 //ADD A NEW MOVIE
 function addMovie() {
